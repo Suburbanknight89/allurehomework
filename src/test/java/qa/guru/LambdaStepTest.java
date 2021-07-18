2dba2eafb11a4757a664ba285f534a622dc73164
suburbanknight89 <lumenteam@gmail.com>
2021-07-18 20:08:41 +0300

    1st test - selenide 2st lambda steps

---
+++ src/test/java/qa/guru/LambdaStepTest.java
@@ -0,0 +1,41 @@
+package qa.guru;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+import static org.openqa.selenium.By.partialLinkText;
+
+public class LambdaStepTest {
+    private static final String REPOSITORY = "eroshenkoam/allure-example";
+    private static final int ISSUE_NUMBER = 68;
+
+    @Test
+    public void restRepositoryIsssue(){
+
+        step("Открываем главную страницу", ()-> {
+                open("https://github.com");
+        });
+
+        step("Ищем репозиторий " + REPOSITORY, ()->{
+            $(".header-search-input").click();
+            $(".header-search-input").sendKeys(REPOSITORY);
+            $(".header-search-input").submit();
+        });
+
+        step("Переходим в раздел Issues", ()-> {
+                $(linkText("eroshenkoam/allure-example")).click();
+        });
+
+        step("Проверяем, что существует Issue с номером "+ ISSUE_NUMBER, ()->{
+            $(partialLinkText("Issues")).click();
+            $(withText("#"+ISSUE_NUMBER)).should(exist);
+        });
+
+    }
+}
+
